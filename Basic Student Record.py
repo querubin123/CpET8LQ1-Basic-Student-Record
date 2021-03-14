c0289dd02626128eae2f3e7e9bfd298f8823552f
querubin123 <80618307+querubin123@users.noreply.github.com>
2021-03-14 16:40:37 +0800

    Initial commit

---
+++ Basic Student Record.py
@@ -0,0 +1,19 @@
+print('BASIC STUDENT RECORD')
+name=input('ENTER YOUR NAME:')
+age=int(input("ENTER YOUR AGE:"))
+gender=input('ENTER YOUR GENDER:')
+number=input("ENTER YOUR ID NUMBER:")
+contact=int(input('ENTER YOUR CONTACT NO:'))
+course=input('ENTER YOUR COURSE:')
+dept=input('ENTER YOUR DEPARTMENT:')
+
+print('NAME: '+name)
+print('AGE: '+str(age))
+print('GENDER: '+gender)
+print('CONTACT NO.: '+str(contact))
+print('COURSE: '+course)
+print('DEPARTMENT: '+dept)
+
+
+
+
